392ca711d6bacd7a991c31d51e4f190bf66f3e82
jiuyu <2506359659@qq.com>
2022-09-20 11:10:17 +0800

    优化登录模块

--- src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -4,6 +4,7 @@ import com.nowcoder.community.entity.LoginTicket;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
+@Deprecated
 public interface LoginTicketMapper {
 
     int insertLoginTicket(LoginTicket loginTicketMapper);
