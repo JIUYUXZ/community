210f5710ecc481d8ad8bfadb39f77643d947986f
jiuyu <2506359659@qq.com>
2022-10-18 18:40:46 +0800

    Spring整合Elasticsearch,开发社区搜索功能

---
+++ src/main/java/com/nowcoder/community/service/ElasticsearchService.java
@@ -0,0 +1,92 @@
+package com.nowcoder.community.service;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ElasticsearchService {
+    @Autowired
+    private DiscussPostRepository discussPostRepository; // 往ES里存、修改、删除数据、搜索可以用到
+
+    @Qualifier("client")
+    @Autowired
+    private RestHighLevelClient restHighLevelClient; // 这个的搜索方法可以做到高亮显示
+
+    // 往ES里存数据(再存一次就是修改)
+    public void saveDiscussPost(DiscussPost post){
+        discussPostRepository.save(post);
+    }
+
+    // 从ES里删除数据
+    public void deleteDiscussPost(int id){
+        discussPostRepository.deleteById(id);
+    }
+
+    // 提供搜索方法并高亮显示  参数1:搜索的关键字, 搜索支持分页,传入分页条件 参数2:当前要显示第几页 参数3:每页显示多少条数据
+    public List<DiscussPost> searchDiscussPost(String keyword, int current, int limit) throws Exception{
+
+        SearchRequest searchRequest = new SearchRequest("discusspost"); // discusspost是索引名，就是表名
+
+        // 高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("title");
+        highlightBuilder.field("content");
+        highlightBuilder.requireFieldMatch(false);
+        highlightBuilder.preTags("<span style='color:red'>");
+        highlightBuilder.postTags("</span>");
+
+
+        //构建搜索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
+                .query(QueryBuilders.multiMatchQuery(keyword, "title", "content"))
+                .sort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .sort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .sort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .from(current)// 指定从哪条开始查询
+                .size(limit)// 需要查出的总记录条数
+                .highlighter(highlightBuilder);
+
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        //获取查询到的总个数
+        //TotalHits totalHits=searchResponse.getHits().getTotalHits();
+
+        List<DiscussPost> list = new LinkedList<>();
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+            DiscussPost discussPost = JSONObject.parseObject(hit.getSourceAsString(), DiscussPost.class);
+
+            // 处理高亮显示的结果
+            HighlightField titleField = hit.getHighlightFields().get("title");
+            if (titleField != null) {
+                discussPost.setTitle(titleField.getFragments()[0].toString());
+            }
+            HighlightField contentField = hit.getHighlightFields().get("content");
+            if (contentField != null) {
+                discussPost.setContent(contentField.getFragments()[0].toString());
+            }
+            System.out.println(discussPost);
+            list.add(discussPost);
+        }
+
+        return list;
+    }
+}
